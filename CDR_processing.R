73335c680a9317b5baf9ab8bb13b592769267681
schaaptamar <97991467+schaaptamar@users.noreply.github.com>
2024-05-29 14:25:59 -0700

    Update CDR_processing.R

--- CDR_processing.R
+++ CDR_processing.R
@@ -1,3 +1,7 @@
+library(dplyr) 
+
 cdr <- read.csv("~/CDR_24Apr2024.csv")
 cdr <- cdr[cdr$CDGLOBAL>=0,]
 cdr$CDRSB <- rowSums(cdr[,c('CDMEMORY','CDORIENT','CDJUDGE','CDCOMMUN','CDHOME','CDCARE')])
+cdr <- cdr %>%
+  dplyr::rename(VISDATE = EXAMDATE)
