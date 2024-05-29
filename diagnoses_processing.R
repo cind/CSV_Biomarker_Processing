d0b7ad194b9e6ca184ee9a851771ea68ff7acf73
schaaptamar <97991467+schaaptamar@users.noreply.github.com>
2024-05-29 14:25:08 -0700

    Create diagnoses_processing.R
    adding file to create CN, MCI, AD

---
+++ diagnoses_processing.R
@@ -0,0 +1,9 @@
+#adding diagnoses as CN, MCI, or AD
+diagnoses <- read.csv("~/DXSUM_PDXCONV_ADNIALL.csv")
+diagnoses[diagnoses == ""] <- NA
+
+diagnoses_all <- diagnoses %>%
+  dplyr::mutate(diagnosis = dplyr::case_when(DIAGNOSIS==1 | DXCHANGE==1 | DXCHANGE==7 | DXCHANGE==9 | DXCURREN == 1 ~ 'CN',
+                                         DIAGNOSIS==2 | DXCHANGE==2 | DXCHANGE==4 | DXCHANGE==8 | DXCURREN == 2 ~ 'MCI',
+                                         DIAGNOSIS==3 | DXCHANGE==3 | DXCHANGE==5 | DXCHANGE==6 | DXCURREN == 3 ~ 'AD')) %>%
+  dplyr::rename(DX.DATE = EXAMDATE)
