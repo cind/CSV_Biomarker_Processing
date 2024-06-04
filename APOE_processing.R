211fcdab0286927d13c1301cf5677d3984a9679e
zhausle <zhausle@umich.edu>
2024-06-04 16:42:46 -0700

    Update APOE_processing.R
    Added table name on LONI

--- APOE_processing.R
+++ APOE_processing.R
@@ -1,3 +1,5 @@
+## table name on LONI: ApoE - Results [ADNI1,GO,2,3]
+
 #this creates a column with both APOE alleles combined
 apoeres <- read.csv("~/APOERES.csv") 
 apoeres$apoe <- paste(paste("E", apoeres$APGEN1, sep = ""), paste("E", apoeres$APGEN2, sep = ""), sep = "/")
