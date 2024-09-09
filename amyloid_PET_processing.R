427a9cd38fc2929ddf14e52fa5c0dc806217f87b
schaaptamar <97991467+schaaptamar@users.noreply.github.com>
2024-09-09 12:58:54 -0700

    Update amyloid_PET_processing.R
    more details about amyloid positivity added

--- amyloid_PET_processing.R
+++ amyloid_PET_processing.R
@@ -1,8 +1,11 @@
 ## table name on LONI: 	UC Berkeley - Amyloid PET 6mm Res analysis [ADNI1,GO,2,3,4]
 
-# Reading in data
+#loading in libraries
+library(dplyr)
+
+# Reading in data - pay attention to quality control -> qc_flag 
 amyloid_pet <- readr::read_delim("~/data/UCBERKELEY_AMY_6MM_31Jul2024.csv")
 amyloid_pet <- amyloid_pet %>% dplyr::rename(suvr_summary=SUMMARY_SUVR,
                                              Centiloid=CENTILOIDS,
-                                             AmyloidPosPET=AMYLOID_STATUS, # amyloid status is based on SUVR - more details explained in methods on LONI
+                                             AmyloidPosPET=AMYLOID_STATUS, # amyloid status is based on SUVR and image resolution - more details explained in methods on LONI
                                              EXAMDATE_pet = SCANDATE)
