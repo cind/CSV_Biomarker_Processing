3871586546ff605190908063efcd6365a64d429a
zhausle <zhausle@umich.edu>
2024-10-21 09:09:44 -0700

    Add files via upload

--- cubbt_biomarkers_processing.R
+++ cubbt_biomarkers_processing.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 
-cubbt_biomarkers <- readr::read_delim("~/Downloads/FNIHBC_BLOOD_BIOMARKER_TRAJECTORIES_16Jul2024.csv") # change path to your path
+cubbt_biomarkers <- readr::read_delim("~/Downloads/All_Subjects_FNIHBC_BLOOD_BIOMARKER_TRAJECTORIES_10Oct2024.csv") # change path to your path
 
 ## split biomarker string into variables with analyte and manufacturer information
 cubbt_biomarkers <- cubbt_biomarkers %>%
@@ -40,8 +40,9 @@ cubbt_biomarkers <- cubbt_biomarkers %>%
 ## generate AB42/40 ratio where not calculated but AB42 and AB40 are available
 cubbt_biomarkers <- cubbt_biomarkers %>%
   dplyr::mutate(Abeta42_Abeta40_plasma_cubbt = case_when(
-    !is.na(Abeta42_Abeta40_plasma_cubbt) ~ Abeta42_Abeta40_plasma_cubbt,
-              !is.na(AB42_plasma_cubbt) & !is.na(AB40_plasma_cubbt) ~ AB42_plasma_cubbt/AB40_plasma_cubbt))
+    !is.na(Abeta42_Abeta40_plasma_cubbt) ~ as.numeric(Abeta42_Abeta40_plasma_cubbt),
+    !is.na(AB42_plasma_cubbt) & !is.na(AB40_plasma_cubbt) & manufacturer_plasma_cubbt == "Roche" ~ as.numeric(AB42_plasma_cubbt)/(as.numeric(AB40_plasma_cubbt)*1000),
+              !is.na(AB42_plasma_cubbt) & !is.na(AB40_plasma_cubbt) ~ as.numeric(AB42_plasma_cubbt)/as.numeric(AB40_plasma_cubbt)))
 
 ## merge duplicates within manufacturer
 cubbt_biomarkers <- cubbt_biomarkers %>%
@@ -49,3 +50,33 @@ cubbt_biomarkers <- cubbt_biomarkers %>%
   dplyr::mutate(across(ends_with("_cubbt") & where(is.numeric),.fns = ~mean(.,na.rm=TRUE))) %>%
   dplyr::ungroup() %>%
   dplyr::distinct_at(vars(RID,VISCODE2,manufacturer_plasma_cubbt),.keep_all = TRUE)
+
+## single table with best biomarkers from preprint
+cubbt_biomarkers_best <- cubbt_biomarkers_long %>%
+  dplyr::filter((manufacturer == "C2N" & analyte %in% c("ptau217_ratio","ptau217"))|
+                (manufacturer == "Fuji" & analyte %in% c("Ab40","Ab42"))|
+                (manufacturer == "Roche" & analyte %in% c("ptau181","NfL","GFAP")))
+
+
+cubbt_biomarkers_best <- cubbt_biomarkers_best %>%
+  tidyr::pivot_wider(.,id_cols = c(RID,VISCODE2,EXAMDATE),
+                     names_from = analyte_clean,
+                     values_from = TESTVALUE)
+
+## add "_plasma_cubbt" tag to variable names
+cubbt_biomarkers_best <- cubbt_biomarkers_best %>%
+  dplyr::rename_with(~paste0(.,"_plasma_cubbt"),
+                     c(colnames(cubbt_biomarkers_best)[4:length(colnames(cubbt_biomarkers_best))]))
+
+## generate AB42/40 ratio where not calculated but AB42 and AB40 are available
+cubbt_biomarkers_best <- cubbt_biomarkers_best %>%
+  dplyr::mutate(Abeta42_Abeta40_plasma_cubbt = case_when(
+    !is.na(AB42_plasma_cubbt) & !is.na(AB40_plasma_cubbt) ~ as.numeric(AB42_plasma_cubbt)/as.numeric(AB40_plasma_cubbt)))
+
+
+## merge duplicates for best table
+cubbt_biomarkers_best <- cubbt_biomarkers_best %>%
+  dplyr::group_by(RID,VISCODE2) %>%
+  dplyr::mutate(across(ends_with("_cubbt") & where(is.numeric),.fns = ~mean(.,na.rm=TRUE))) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct_at(vars(RID,VISCODE2),.keep_all = TRUE)
