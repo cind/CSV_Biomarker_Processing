7e505939440fc8e35a97fd7737f4b984c1b68637
zhausle <zhausle@umich.edu>
2024-12-04 20:27:19 -0500

    Add files via upload

--- amyloid_PET_processing.R
+++ amyloid_PET_processing.R
@@ -1,11 +1,20 @@
-## table name on LONI: 	UC Berkeley - Amyloid PET 6mm Res analysis [ADNI1,GO,2,3,4]
+## table name on LONI: UC Berkeley - Amyloid PET 6mm Res analysis [ADNI1,GO,2,3,4]
 
-#loading in libraries
-library(dplyr)
+library(tidyverse)
 
-# Reading in data - pay attention to quality control -> qc_flag 
-amyloid_pet <- readr::read_delim("~/data/UCBERKELEY_AMY_6MM_31Jul2024.csv")
-amyloid_pet <- amyloid_pet %>% dplyr::rename(suvr_summary=SUMMARY_SUVR,
-                                             Centiloid=CENTILOIDS,
-                                             AmyloidPosPET=AMYLOID_STATUS, # amyloid status is based on SUVR and image resolution - more details explained in methods on LONI
-                                             EXAMDATE_pet = SCANDATE)
+amyloid_pet <-readr::read_delim("~/Downloads/UCBERKELEY_AMY_6MM_04Dec2024.csv") ## change to your path
+
+amyloid_pet <- amyloid_pet %>%
+  dplyr::mutate(
+    amyloid_pet_cs_pos = AMYLOID_STATUS, ## use amyloid_pet_cs_pos for cross-sectional analyses
+    amyloid_pet_long_pos = AMYLOID_STATUS_COMPOSITE_REF, ## use amyloid_pet_long_pos for longitudinal analyses
+    amyloid_pet_centiloid_pos = as.numeric(CENTILOIDS > 22))
+
+amyloid_pet_all_regions <- amyloid_pet
+
+amyloid_pet <- amyloid_pet %>%
+  dplyr::select(RID,VISCODE,SCANDATE,
+                amyloid_pet_cs_pos,amyloid_pet_long_pos,amyloid_pet_centiloid_pos,
+                CENTILOIDS,SUMMARY_SUVR,SUMMARY_VOLUME,
+                COMPOSITE_REF_SUVR,COMPOSITE_REF_VOLUME)
+
