80e6871711a1acfec765e9499a3c73d0e384263c
Isabella Hausle <zhausle@umich.edu>
2026-05-18 21:01:19 -0700

    Add files via upload

--- demographics_processing.R
+++ demographics_processing.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 
-demographics <- readr::read_delim("~/Downloads/PTDEMOG_21Apr2025.csv") # change path to your path
+demographics <- readr::read_delim("~/Downloads/PTDEMOG_26Apr2026.csv") # change path to your path
 
 demographics <- demographics %>% dplyr::select(RID,VISCODE,VISCODE2,VISDATE,
                                                PTGENDER,PTDOB,PTDOBYY,PTEDUCAT,
@@ -26,19 +26,24 @@ demographics <- demographics %>%
       PTRACCAT==5 ~ "White",
       PTRACCAT==6 ~ "More than one race",
       PTRACCAT==7 ~ "Unknown"),
-    PTEDUCAT = case_when(
-      PTEDUCAT > 17 ~ "Any postgraduate degree",
+    education_cat = case_when(
+      PTEDUCAT > 16 ~ "Any postgraduate education",
       PTEDUCAT == 16 ~ "Bachelor's",
-      PTEDUCAT == 14 ~ "Associate's",
       PTEDUCAT == 12 ~ "High school or GED",
+      PTEDUCAT > 12 ~ "Associate's/Some college",
       PTEDUCAT > 0 ~ "Less than high school"
     ),
+    education_cat = factor(education_cat,levels = c("Less than high school",
+                                                    "High school or GED",
+                                                    "Associate's/Some college",
+                                                    "Bachelor's",
+                                                    "Any postgraduate education")),
     PTDOBMM = stringr::str_split_i(PTDOB,"/",1)
       )
 
 ## calculate age based on the difference in the month and year terms between the subject's birthdate and the visit date
 demographics$age_bl <-round(as.numeric(lubridate::year(demographics$VISDATE)) -
-                              as.numeric(demographics$PTDOBYY) +
+                              as.numeric(lubridate::year(demographics$PTDOBYY)) +
           ((as.numeric(lubridate::month(demographics$VISDATE)) - 
               as.numeric(demographics$PTDOBMM)) / 12), digits=1)
 
@@ -46,14 +51,16 @@ demographics <- demographics %>%
   dplyr::rename(sex = PTGENDER,
                birth_year = PTDOBYY,
                birth_month = PTDOBMM,
-               education_cat = PTEDUCAT,
                ethnicity = PTETHCAT,
                race = PTRACCAT,
                demog_date = VISDATE)
+
+demographics <- demographics %>%
+  dplyr::mutate(birth_year = lubridate::year(birth_year))
 
 demographics <- demographics %>%
   dplyr::arrange(demog_date) %>%
   dplyr::distinct_at(vars(RID),.keep_all=TRUE)
 
 demographics <- demographics %>%
-  dplyr::select(RID,VISCODE2,sex,age_bl,education_cat,ethnicity,race,birth_year,birth_month,demog_date)
+  dplyr::select(RID,VISCODE2,sex,age_bl,education_cat,PTEDUCAT,ethnicity,race,birth_year,birth_month,demog_date)
