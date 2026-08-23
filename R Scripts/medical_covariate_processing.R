8b53f43b1e98ece44f5d0016264f07b35081dc28
Isabella Hausle <zhausle@umich.edu>
2026-08-23 13:30:33 -0700

    Add files via upload

--- R Scripts/medical_covariate_processing.R
+++ R Scripts/medical_covariate_processing.R
@@ -2,11 +2,11 @@
 ## also requires vitals_processing.R, egfr_processing.R, demographics_processing.R, and plasma_labdata_processing.R
 
 library(tidyverse)
-source("~/vitals_processing.R")
-source("~/egfr_processing.R")
+source("~/Downloads/vitals_processing.R")
+source("~/Downloads/plasma_labdata_processing.R")
 
 ## variables for history of hypertension or stroke at baseline from the Hachinski Ischemic Index
-hachinski <- readr::read_delim("~/Downloads/MODHACH_03Dec2024.csv") %>%
+hachinski <- readr::read_delim("~/Downloads/MODHACH_10Aug2026.csv") %>%
   dplyr::mutate(hypertension_bl_ind = case_when(
     HMHYPERT == 1 ~ 1,
     TRUE ~ 0
@@ -23,8 +23,4 @@ medical_covariates <- dplyr::full_join(vitals,hachinski %>%
 
 medical_covariates <- dplyr::full_join(medical_covariates,
                                        lab_data,
-                                       by = "RID") 
-
-medical_covariates <- dplyr::full_join(medical_covariates,
-                                       egfr_df,
-                                       by = "RID") 
+                                       by = c("RID","VISCODE3"))
--- R Scripts/plasma_labdata_processing.R
+++ R Scripts/plasma_labdata_processing.R
@@ -1,6 +1,29 @@
 library(tidyverse)
+library(transplantr)
 
-lab_data <- readr::read_delim("~/Downloads/All_Subjects_LABDATA_10Oct2024.csv")
+source("~/Downloads/demographics_processing.R")
+source("~/Downloads/adni_generate_VISCODE3_fn.R")
+
+lab_data <- readr::read_delim("~/Downloads/LABDATA_10Aug2026.csv")
+
+urmc_df <- readr::read_delim("~/Downloads/URMC_LABDATA_10Aug2026.csv")
+
+urmc_df <- urmc_df %>%
+  dplyr::filter(TestID %in% c("CRE2","GLU","TP","CHOL"),
+                TestName %in% c("Creatinine","Glucose","Total Protein","Cholesterol")) %>%
+  dplyr::select(RID,VISCODE2,SampleDate,TestID,TestName,ResultValue) %>%
+  tidyr::pivot_wider(names_from = TestName,
+                     values_from = ResultValue,
+                     id_cols = c(RID,VISCODE2,SampleDate)) %>%
+  dplyr::mutate(
+    cholesterol = as.numeric(Cholesterol),
+    creatinine = as.numeric(Creatinine),
+    total_protein = as.numeric('Total Protein'),
+    glucose = as.numeric(Glucose)
+  ) %>%
+  dplyr::rename(lab_date = SampleDate) %>%
+  dplyr::select(RID,VISCODE2,lab_date,
+                cholesterol,creatinine,total_protein,glucose)
 
 lab_data <- lab_data %>%
   dplyr::mutate(glucose = as.numeric(RCT11),
@@ -31,7 +54,59 @@ lab_data <- lab_data %>%
                    total_protein = mean(total_protein,na.rm=TRUE),
                    creatinine = mean(creatinine,na.rm=TRUE),
                    cholesterol = mean(cholesterol,na.rm=TRUE),
-                   lab_date = as.Date(mean(EXAMDATE,na.rm=TRUE))) %>%
+                   lab_date = as.Date(mean(EXAMDATE,na.rm=TRUE)))
+
+lab_data <- dplyr::left_join(lab_data,demographics,by="RID")
+
+lab_data$age_at_lab <-
+  round(as.numeric(lubridate::year(
+    lab_data$lab_date
+  )) -
+    lab_data$birth_year +
+    ((
+      as.numeric(
+        lubridate::month(lab_data$lab_date)
+      ) - as.numeric(lab_data$birth_month)
+    ) / 12),
+  1)
+
+lab_data$race_for_egfr <-
+  ifelse(
+    lab_data$race == "Black or African American",
+    "black",
+    "non-black"
+  )
+
+lab_data$gender_for_egfr <-
+  ifelse(
+    lab_data$sex == "Male",
+    "M",
+    "F"
+  )
+
+lab_data$eGFR <-
+  transplantr::ckd_epi(
+    lab_data$creatinine,
+    lab_data$age_at_lab,
+    lab_data$gender_for_egfr,
+    lab_data$race_for_egfr,
+    units = "US"
+  )
+
+lab_data <- lab_data %>%
+  dplyr::select(RID,glucose,total_protein,creatinine,cholesterol,
+                lab_date,eGFR
+  )
+
+## read in registry to calculate alternate VISCODE (VISCODE3)
+registry <- readr::read_delim("~/Downloads/REGISTRY_10Aug2026.csv") # change path to your path
+
+lab_data <- adni_generate_VISCODE3(df = lab_data,registry_df = registry,
+                                 value_cols = c("glucose","total_protein",
+                                                "creatinine","cholesterol"),
+                                 date_col = lab_data$lab_date)
+
+lab_data <- lab_data %>%
   dplyr::mutate(diabetes_bl_ind = as.factor(case_when(
     glucose > 125 ~ 1,
     glucose <= 125 ~ 0
@@ -39,4 +114,7 @@ lab_data <- lab_data %>%
   hyperlipidemia_bl_ind = as.factor(case_when(
     cholesterol > 200 ~ 1,
     cholesterol <= 200 ~ 0
-  )))
+  )),
+  ckd_ind = as.factor(case_when(
+    (eGFR <= 60) ~ 1,
+    eGFR > 60 ~ 0)))
